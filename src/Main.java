b15538f28cac03e39885f48ff9dfcb96b9c82283
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-05-08 16:06:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1000;
+        int coins = 20;
+        int miles = 1;
+        int bonus = price / coins * miles;
+        System.out.println(bonus);
+    }
+}
